f519c7ceaf8410861cf4e3f3f7a73942580057e6
MarcosHM851 <marcoshm2005@gmail.com>
2025-03-22 12:38:29 +0100

    He organizado en paquetes la mayoria de las clases

---
+++ Grupo6/src/Desafio/Request.java
@@ -0,0 +1,14 @@
+package Grupo6.src.Desafio;
+
+import Grupo6.src.App.Operador;
+
+public class Request {
+
+    private Desafio desafio;
+    private boolean isValid;
+    private Operador operator;
+
+    public Request(){
+
+    }
+}
